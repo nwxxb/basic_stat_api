4430114f391cf1f20d83d4ab3c384984b5985a99
nawwab <nawwabzia@gmail.com>
2023-10-12 15:19:57 +0700

    rescue error when data input is invalid

--- app/routes.rb
+++ app/routes.rb
@@ -17,60 +17,72 @@ module BasicStatApi
         mean = data.sum(0.0) / data.length
         return { mean: }.to_json
       rescue JSON::GeneratorError, NoMethodError
-        error 400
+        error 400, { error: 'invalid input' }.to_json
       end
     end
 
     post '/median' do
       content_type 'application/json'
 
-      data = JSON.parse(request.body.read)['data']
-      sorted_values = data.sort
-      median = if sorted_values.length.odd?
-                 sorted_values[((sorted_values.length + 1) / 2) - 1]
-               else
-                 [
-                   sorted_values[((sorted_values.length + 1) / 2.0).ceil - 1],
-                   sorted_values[(sorted_values.length / 2) - 1]
-                 ].sum(0.0) / 2
-               end
+      begin
+        data = JSON.parse(request.body.read)['data']
+        sorted_values = data.sort
+        median = if sorted_values.length.odd?
+                   sorted_values[((sorted_values.length + 1) / 2) - 1]
+                 else
+                   [
+                     sorted_values[((sorted_values.length + 1) / 2.0).ceil - 1],
+                     sorted_values[(sorted_values.length / 2) - 1]
+                   ].sum(0.0) / 2
+                 end
 
-      return { median: }.to_json
+        return { median: }.to_json
+      rescue JSON::GeneratorError, NoMethodError
+        error 400, { error: 'invalid input' }.to_json
+      end
     end
 
     post '/mode' do
       content_type 'application/json'
 
-      data = JSON.parse(request.body.read)['data']
-      value_count = data.map
-                        .each_with_object({}) do |val, counter|
-        counter[val] = if counter[val].nil?
-                         1
-                       else
-                         counter[val] + 1
-                       end
+      begin
+        data = JSON.parse(request.body.read)['data']
+        value_count = data.map
+                          .each_with_object({}) do |val, counter|
+          counter[val] = if counter[val].nil?
+                           1
+                         else
+                           counter[val] + 1
+                         end
+        end
+
+        mode = value_count.select do |_, value|
+          value == value_count.values.max
+        end.keys
+
+        return { mode: }.to_json
+      rescue JSON::GeneratorError, NoMethodError
+        error 400, { error: 'invalid input' }.to_json
       end
-
-      mode = value_count.select do |_, value|
-        value == value_count.values.max
-      end.keys
-
-      return { mode: }.to_json
     end
 
     post '/standard_deviation' do
       content_type 'application/json'
 
-      data, sample_flag = JSON.parse(request.body.read).values
-      mean = data.sum(0.0) / data.length
-      length = sample_flag ? data.length - 1 : data.length
-      standard_deviation = Math.sqrt(
-        data.map do |val|
-          ((val.to_f - mean)**2)
-        end.sum / length
-      )
+      begin
+        data, sample_flag = JSON.parse(request.body.read).values
+        mean = data.sum(0.0) / data.length
+        length = sample_flag ? data.length - 1 : data.length
+        standard_deviation = Math.sqrt(
+          data.map do |val|
+            ((val.to_f - mean)**2)
+          end.sum / length
+        )
 
-      return { standard_deviation: }.to_json
+        return { standard_deviation: }.to_json
+      rescue JSON::GeneratorError, NoMethodError
+        error 400, { error: 'invalid input' }.to_json
+      end
     end
 
     get '/' do
