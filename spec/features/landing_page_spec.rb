6977cce6b8b42ec23e8a1dc9a29fdf523a5818ad
nawwab <nawwabzia@gmail.com>
2023-11-11 16:25:28 +0700

    update describe and test for title presence

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe '/median endpoint', type: :feature do
+RSpec.describe 'landing page', type: :feature do
   def app
     Rack::Builder.new do
       map('/api') { run BasicStatApi::Calculations }
@@ -8,14 +8,22 @@ RSpec.describe '/median endpoint', type: :feature do
     end
   end
 
-  it 'give correct demo', js: true do
+  before do
     Capybara.app = app
+  end
+
+  it 'has title' do
+    visit('/')
+    expect(page).to have_content(/Basic.+Stat.+Api/)
+  end
+
+  it 'provide functional demo', js: true do
     visit('/')
     fill_in 'Data Payload', with: '60, 70, 100, 80, 50, 50, 75'
     click_button 'Submit'
-    expect(page).to have_content(/mean.+\:.+69\.285/)
-    expect(page).to have_content(/median.+\:.+70/)
-    expect(page).to have_content(/mode.+\:.+50/)
-    expect(page).to have_content(/standard_deviation.+\:.+16.5677/)
+    expect(page).to have_content(/mean.+:.+69\.285/)
+    expect(page).to have_content(/median.+:.+70/)
+    expect(page).to have_content(/mode.+:.+50/)
+    expect(page).to have_content(/standard_deviation.+:.+16.5677/)
   end
 end
